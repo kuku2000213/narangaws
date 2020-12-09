9ae2f6d0db684b5f268b5689d0d8510dc59ce07c
kuku2000213 <nd10@narangdesign.com>
2020-12-09 20:08:08 +0900

    first

---
+++ tjtech/src/main/java/com/tech/taejin/TjtechApplication.java
@@ -0,0 +1,13 @@
+package com.tech.taejin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TjtechApplication.class, args);
+	}
+
+}
---
+++ tjtech/src/test/java/com/tech/taejin/TjtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.taejin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
